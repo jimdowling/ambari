c26ae191c39c0488ad2d57401459962d1cfb4386
Jim Dowling <jdowling@sics.se>
2015-04-11 10:16:44 +0200

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,16 @@
 name             'ambari'
 maintainer       'JULIEN PELLET'
 maintainer_email 'chef@julienpellet.com'
+additional_author 'Jim Dowling'
 license          'Apache 2.0'
 description      'Installs/Configures ambari'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.11'
+
+recipe           "ambari::server", "Installs an Ambari Server"
+recipe           "ambari::agent", "Installs an Ambari agent"
+recipe           "ambari::blueprints", "Installs an Ambari blueprint"
+
 supports 'redhat', ">= 5.0"
 supports 'centos', ">= 5.0"
 supports 'amazon', ">= 5.0"
@@ -12,10 +18,6 @@ supports 'scientific', ">= 5.0"
 supports 'suse', ">= 11.0"
 supports 'ubuntu', ">= 12.0"
 depends 'apt'
-
-recipe   "ambari::server", "Installs an Ambari Server"
-recipe   "ambari::agent", "Installs an Ambari agent"
-recipe   "ambari::blueprints", "Installs an Ambari blueprint"
 
 
 attribute "ambari/admin_password"
